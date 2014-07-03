1259f03f500815667608a58873f73bd89f15f9f2
thepeg <marco.pegoraro@gmail.com>
2014-07-03 19:46:47 +0200

    header slides away while searching

--- src/features/footer-ui/index.js
+++
@@ -1,9 +0,0 @@
-
-var layoutEngine = require('layout-engine');
-
-var viewModel = require('./view-model');
-var template = require('./template.html');
-
-exports.start = function(config) {
-    layoutEngine.render(template, viewModel(), 'footer');
-};
